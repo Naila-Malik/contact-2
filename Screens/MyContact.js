5479fd4d5ddee3ea398b1ba9d3a12ac2ecd040e6
unknown <nailaamalik226@gmail.com>
2022-07-20 00:05:19 +0500

    2nd commit

---
+++ Screens/MyContact.js
@@ -0,0 +1,64 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import Ionicons from 'react-native-vector-icons/AntDesign';
+import Contacts from 'react-native-contacts';
+import {useIsFocused} from '@react-navigation/native';
+import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
+import {PermissionsAndroid} from 'react-native';
+import ContactCard from './ContactCard';
+
+
+
+
+export default function MyContact({navigation}) {
+  const isFocused = useIsFocused();
+
+  const [myContacts, setMyContacts] = useState([]);
+
+
+  useEffect(() => {
+    getAllContacts();
+  }, [isFocused]);
+
+  const getAllContacts = async () => {
+    try {
+      const permission = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      );
+      if (permission === 'granted') {
+        const contacts = await Contacts.getAll();
+          setMyContacts(contacts);
+          // console.log("LIstof contacts", contacts)
+      }
+      else { setMyContacts([]) }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View>
+      <Ionicons
+        name="pluscircle"
+        size={52}
+        color="green"
+        onPress={() => navigation.navigate('CreateContact')}
+      />
+      <FlatList 
+      data = {myContacts}
+      keyExtractor = {(item) => item.recordID}
+      renderItem = {({item}) => (
+        <TouchableOpacity onPress={() => navigation.navigate('Profile' , {
+          contactInfo : {id : item.recordID},
+        })} >
+          <ContactCard contactInfo = {item}  />
+        </TouchableOpacity>
+      )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
